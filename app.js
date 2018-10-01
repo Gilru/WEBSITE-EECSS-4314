51b43f920f0a6aa7bc2ef9665d83fc1388666eb6
GILBERT ANDZE <gilberttsoungui@gmail.com>
2018-10-01 01:35:24 -0400

    heroku awake

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express()
 
+const http = require("http");
+
 app.get("/",function (req,res) {
     res.sendFile(__dirname + "/index.html")
 })
@@ -9,3 +11,10 @@ app.listen((process.env.PORT || 3000),function(){
   console.log("EECS 4314 listening on port 3000!")
 
 })
+
+
+
+/**********keep awake********************/
+setInterval(function() {
+    http.get("https://eecs-4314.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
